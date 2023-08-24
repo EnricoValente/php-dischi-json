9ac3b296684b98c919834848cbe1d9f83d27a985
EnricoValente <enricovalente42@gmail.com>
2023-08-24 18:24:37 +0200

    edit riusltati API in pagina

--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            disks:[]
         }
+            
     },
     created() {
        axios
             .get('http://localhost/php-dischi-json/api.php')
             .then(response => {
                 console.log(response.data);
+                this.disks = response.data
             });
     }
 }).mount('#app');
